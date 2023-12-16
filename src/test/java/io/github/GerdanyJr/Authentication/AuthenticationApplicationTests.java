a442494b8474bc151bc1871dba2754f9c0e90e8d
Gerdany Junior <Asa_lindo@hotmail.com>
2023-12-16 16:41:46 -0300

    initial commit

---
+++ src/test/java/io/github/GerdanyJr/Authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.GerdanyJr.Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
